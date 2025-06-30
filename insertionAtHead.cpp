559b099bfc328f424dffc45de615f69a2798c4a4
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-30 21:22:46 +0600

    Inserted a node at head of a linked list

---
+++ insertionAtHead.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int integerValue;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->nextPointer = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int newNumber)
+{
+    Node *newNode = new Node(newNumber);
+    newNode->nextPointer = head;
+    head = newNode;
+}
+
+void printLinkedList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->integerValue << endl;
+        tmp = tmp->nextPointer;
+    }
+}
+
+int main()
+{
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+
+    int newNumber;
+    cin >> newNumber;
+
+    Node *head = new Node(num1);
+    Node *secondNode = new Node(num2);
+    Node *tail = new Node(num3);
+
+    head->nextPointer = secondNode;
+    secondNode->nextPointer = tail;
+
+    insertAtHead(head, newNumber);
+    printLinkedList(head);
+
+    return 0;
+}
