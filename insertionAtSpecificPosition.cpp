6c8dbc975901bb8ac7c92c516d52bfba867f5561
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-07-01 14:16:12 +0600

    Inserted at specific position of linked list

--- insertionAtSpecificPosition.cpp
+++ insertionAtSpecificPosition.cpp
@@ -14,17 +14,46 @@ public:
     }
 };
 
+void insertAtAnyPosition(Node *&head, int desiredIndex, int integerValue)
+{
+    Node *newNode = new Node(integerValue);
+    Node *tmp = head;
+    for (int i = 1; i < desiredIndex; i++)
+    {
+        tmp = tmp->nextPointer;
+    }
+
+    newNode->nextPointer = tmp->nextPointer;
+    tmp->nextPointer = newNode;
+}
+
+void printLinkedList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->integerValue << endl;
+        tmp = tmp->nextPointer;
+    }
+}
+
 int main()
 {
     int num1, num2, num3;
     cin >> num1 >> num2 >> num3;
 
-    int num4;
-    cin >> num4;
+    int num4, num5;
+    cin >> num4 >> num5;
 
     Node *head = new Node(num1);
     Node *secondNode = new Node(num2);
     Node *tail = new Node(num3);
+
+    head->nextPointer = secondNode;
+    secondNode->nextPointer = tail;
+
+    insertAtAnyPosition(head, num4, num5);
+    printLinkedList(head);
 
     return 0;
 }
